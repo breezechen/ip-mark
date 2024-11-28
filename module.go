ac8d0e7883a5c36931dd25a3dc9088c1ecead3f4
Breeze Chen <breezechen356@foxmail.com>
2024-11-28 14:39:24 +0800

    add logging

--- module.go
+++ module.go
@@ -1,107 +1,144 @@
 package ipmark
 
 import (
-	"net/http"
-	"sync"
+    "fmt"
+    "net/http"
+    "sync"
 
-	"github.com/caddyserver/caddy/v2"
-	"github.com/caddyserver/caddy/v2/caddyconfig/httpcaddyfile"
-	"github.com/caddyserver/caddy/v2/modules/caddyhttp"
+    "github.com/caddyserver/caddy/v2"
+    "github.com/caddyserver/caddy/v2/caddyconfig/httpcaddyfile"
+    "github.com/caddyserver/caddy/v2/modules/caddyhttp"
+    "go.uber.org/zap"
 )
 
 func init() {
-	caddy.RegisterModule(MarkIP{})
-	caddy.RegisterModule(IPMatcher{})
-	httpcaddyfile.RegisterHandlerDirective("mark_ip", parseCaddyfileMarkIP)
-	httpcaddyfile.RegisterMatcherDirective("match_ip", parseCaddyfileMatchIP)
+    caddy.RegisterModule(MarkIP{})
+    caddy.RegisterModule(IPMatcher{})
+    httpcaddyfile.RegisterHandlerDirective("mark_ip", parseCaddyfileMarkIP)
+    httpcaddyfile.RegisterMatcherDirective("match_ip", parseCaddyfileMatchIP)
 }
 
 // IPStore is a global store for IPs
 var (
-	ipStore     = make(map[string]struct{})
-	ipStoreLock sync.RWMutex
+    ipStore     = make(map[string]struct{})
+    ipStoreLock sync.RWMutex
 )
 
 // MarkIP implements an HTTP handler that stores IPs
-type MarkIP struct{}
+type MarkIP struct {
+    logger *zap.Logger
+}
 
 // IPMatcher implements a request matcher for stored IPs
-type IPMatcher struct{}
+type IPMatcher struct {
+    logger *zap.Logger
+}
 
 // CaddyModule returns the Caddy module information.
 func (MarkIP) CaddyModule() caddy.ModuleInfo {
-	return caddy.ModuleInfo{
-		ID:  "http.handlers.mark_ip",
-		New: func() caddy.Module { return new(MarkIP) },
-	}
+    return caddy.ModuleInfo{
+        ID:  "http.handlers.mark_ip",
+        New: func() caddy.Module { return new(MarkIP) },
+    }
 }
 
 func (IPMatcher) CaddyModule() caddy.ModuleInfo {
-	return caddy.ModuleInfo{
-		ID:  "http.matchers.match_ip",
-		New: func() caddy.Module { return new(IPMatcher) },
-	}
+    return caddy.ModuleInfo{
+        ID:  "http.matchers.match_ip",
+        New: func() caddy.Module { return new(IPMatcher) },
+    }
+}
+
+// Provision sets up the module
+func (m *MarkIP) Provision(ctx caddy.Context) error {
+    m.logger = ctx.Logger(m)
+    return nil
+}
+
+// Provision sets up the module
+func (m *IPMatcher) Provision(ctx caddy.Context) error {
+    m.logger = ctx.Logger(m)
+    return nil
 }
 
 // ServeHTTP implements caddyhttp.MiddlewareHandler.
-func (m MarkIP) ServeHTTP(w http.ResponseWriter, r *http.Request, next caddyhttp.Handler) error {
-	ip := getRealIP(r)
-	if ip != "" {
-		ipStoreLock.Lock()
-		ipStore[ip] = struct{}{}
-		ipStoreLock.Unlock()
-	}
-	return next.ServeHTTP(w, r)
+func (m *MarkIP) ServeHTTP(w http.ResponseWriter, r *http.Request, next caddyhttp.Handler) error {
+    ip := getRealIP(r)
+    if ip != "" {
+        ipStoreLock.Lock()
+        ipStore[ip] = struct{}{}
+        ipStoreLock.Unlock()
+        m.logger.Info("marked ip",
+            zap.String("ip", ip),
+            zap.String("path", r.URL.Path),
+            zap.String("method", r.Method))
+    } else {
+        m.logger.Warn("could not determine real IP",
+            zap.String("path", r.URL.Path),
+            zap.String("method", r.Method))
+    }
+    return next.ServeHTTP(w, r)
 }
 
 // Match implements caddyhttp.RequestMatcher
-func (m IPMatcher) Match(r *http.Request) bool {
-	ip := getRealIP(r)
-	if ip == "" {
-		return false
-	}
+func (m *IPMatcher) Match(r *http.Request) bool {
+    ip := getRealIP(r)
+    if ip == "" {
+        m.logger.Debug("no IP found in request",
+            zap.String("path", r.URL.Path),
+            zap.String("method", r.Method))
+        return false
+    }
 
-	ipStoreLock.RLock()
-	_, exists := ipStore[ip]
-	ipStoreLock.RUnlock()
+    ipStoreLock.RLock()
+    _, exists := ipStore[ip]
+    ipStoreLock.RUnlock()
 
-	return exists
+    m.logger.Debug("checking IP match",
+        zap.String("ip", ip),
+        zap.String("path", r.URL.Path),
+        zap.String("method", r.Method),
+        zap.Bool("matched", exists))
+
+    return exists
 }
 
 // getRealIP implements the IP resolution logic
 func getRealIP(r *http.Request) string {
-	// Try Cloudflare IP
-	if ip := r.Header.Get("Cf-Connecting-Ip"); ip != "" {
-		return ip
-	}
-	// Try X-Forwarded-For
-	if ip := r.Header.Get("X-Forwarded-For"); ip != "" {
-		return ip
-	}
-	// Try X-Real-Ip
-	if ip := r.Header.Get("X-Real-Ip"); ip != "" {
-		return ip
-	}
-	// Use Remote Address
-	return r.RemoteAddr
+    // Try Cloudflare IP
+    if ip := r.Header.Get("Cf-Connecting-Ip"); ip != "" {
+        return ip
+    }
+    // Try X-Forwarded-For
+    if ip := r.Header.Get("X-Forwarded-For"); ip != "" {
+        return ip
+    }
+    // Try X-Real-Ip
+    if ip := r.Header.Get("X-Real-Ip"); ip != "" {
+        return ip
+    }
+    // Use Remote Address
+    return r.RemoteAddr
 }
 
 // parseCaddyfileMarkIP unmarshals tokens from h into a new middleware handler
 func parseCaddyfileMarkIP(h httpcaddyfile.Helper) (caddyhttp.MiddlewareHandler, error) {
-	var m MarkIP
-	return m, nil
+    var m MarkIP
+    return &m, nil
 }
 
 // parseCaddyfileMatchIP unmarshals tokens from h into a new matcher
 func parseCaddyfileMatchIP(h httpcaddyfile.Helper) (caddyhttp.RequestMatcher, error) {
-	var m IPMatcher
-	return m, nil
+    var m IPMatcher
+    return &m, nil
 }
 
 // Interface guards
 var (
-	_ caddy.Module                = (*MarkIP)(nil)
-	_ caddy.Module                = (*IPMatcher)(nil)
-	_ caddyhttp.MiddlewareHandler = (*MarkIP)(nil)
-	_ caddyhttp.RequestMatcher    = (*IPMatcher)(nil)
+    _ caddy.Module                = (*MarkIP)(nil)
+    _ caddy.Module                = (*IPMatcher)(nil)
+    _ caddyhttp.MiddlewareHandler = (*MarkIP)(nil)
+    _ caddyhttp.RequestMatcher    = (*IPMatcher)(nil)
+    _ caddy.Provisioner           = (*MarkIP)(nil)
+    _ caddy.Provisioner           = (*IPMatcher)(nil)
 )
